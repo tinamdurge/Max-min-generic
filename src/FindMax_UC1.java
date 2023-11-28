05f780b5fe5faefcdb6623eed6a9e3f825977e11
Tinam Durge <tinamdurge02@gmail.com>
2023-11-28 15:57:41 +0530

    Maximum generic

---
+++ src/FindMax_UC1.java
@@ -0,0 +1,39 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class FindMax_UC1 {
+    static Integer  a,b,c;
+    public void getInput() {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter First Value: ");
+        a = s.nextInt();
+
+        System.out.print("Enter Second Value: ");
+        b = s.nextInt();
+
+        System.out.print("Enter Third Value: ");
+        c = s.nextInt();
+
+    }
+    public Integer testMaximum(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if(y.compareTo(max)>0) {
+            max = y;
+        }
+        if(z.compareTo(max)>0)
+        {
+            max = z;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        FindMax_UC1 u = new FindMax_UC1();
+        Integer max ;
+        u.getInput();
+        max = u.testMaximum(a, b, c);
+        System.out.println("Maxmum value is : " + max);
+
+    }
+}
